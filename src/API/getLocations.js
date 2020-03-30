3a6c73d293265259713640e5bb25d84d4b6a8cde
Yael Stein <yaelst@gmail.com>
2020-03-30 17:20:18 +0200

    improve request to get more berlin locations

--- src/API/getLocations.js
+++ src/API/getLocations.js
@@ -2,7 +2,7 @@
 
 export const getLocations = () => {
 
-  return fetch(' https://api.openchargemap.io/v3/poi/?output=json&countrycode=DE&maxresults=5000&compact=true&verbose=false').then(response => {
+  return fetch(' https://api.openchargemap.io/v3/poi/?output=json&countrycode=DE&maxresults=80000&compact=true&verbose=false&latitude=52.5200&longitude=13.4050').then(response => {
     return response.json();
   }).then(data => {
     console.log(data);
